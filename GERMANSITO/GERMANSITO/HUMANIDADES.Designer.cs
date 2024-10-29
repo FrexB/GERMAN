c7bf112ac280d1e493b5eee65da3537b9a508b71
Ramsesballesteros <badr080626hmnlzma7@cbtis217.edu.mx>
2024-10-29 08:34:15 -0600

    se agrego la ventana de humanidades

---
+++ GERMANSITO/GERMANSITO/HUMANIDADES.Designer.cs
@@ -0,0 +1,139 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC26
+ * Date: 29/10/2024
+ * Time: 08:07 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace GERMANSITO
+{
+	partial class HUMANIDADES
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HUMANIDADES));
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(188, 4);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(162, 30);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "HUMANIDADES ";
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(-13, 37);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(543, 303);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.PictureBox1Click);
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(42, 252);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(89, 27);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "FILOSOFIA ";
+			this.label2.Click += new System.EventHandler(this.Label2Click);
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(148, 252);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(79, 27);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "HISTORIA ";
+			this.label3.Click += new System.EventHandler(this.Label3Click);
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(255, 252);
+			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(95, 27);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "LITERATURA";
+			this.label4.Click += new System.EventHandler(this.Label4Click);
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(382, 252);
+			this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(86, 27);
+			this.label5.TabIndex = 5;
+			this.label5.Text = "RELIGION ";
+			this.label5.Click += new System.EventHandler(this.Label5Click);
+			// 
+			// HUMANIDADES
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(512, 337);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.Name = "HUMANIDADES";
+			this.Text = "HUMANIDADES";
+			this.Load += new System.EventHandler(this.HUMANIDADESLoad);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ GERMANSITO/GERMANSITO/HUMANIDADES.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC26
+ * Date: 29/10/2024
+ * Time: 08:07 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GERMANSITO
+{
+	/// <summary>
+	/// Description of HUMANIDADES.
+	/// </summary>
+	public partial class HUMANIDADES : Form
+	{
+		public HUMANIDADES()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void PictureBox1Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void Label3Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void Label5Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void Label1Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void HUMANIDADESLoad(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void Label4Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void Label2Click(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
