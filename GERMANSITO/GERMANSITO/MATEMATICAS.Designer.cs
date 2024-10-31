9f45a88381f9499501e42120c601d0b9289355ad
camilaorozc0 <niod080216mgttrna6@cbtis217.edu.mx>
2024-10-31 08:21:38 -0600

    Prueba subiendo matematicas sin carpetas debug

--- GERMANSITO/GERMANSITO/MATEMATICAS.Designer.cs
+++ GERMANSITO/GERMANSITO/MATEMATICAS.Designer.cs
@@ -36,12 +36,309 @@ namespace GERMANSITO
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MATEMATICAS));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.Ecosistemas = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.label10 = new System.Windows.Forms.Label();
+			this.textBox9 = new System.Windows.Forms.TextBox();
+			this.label11 = new System.Windows.Forms.Label();
+			this.textBox10 = new System.Windows.Forms.TextBox();
+			this.label12 = new System.Windows.Forms.Label();
+			this.textBox11 = new System.Windows.Forms.TextBox();
+			this.label13 = new System.Windows.Forms.Label();
+			this.textBox12 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(577, 62);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(286, 177);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(104, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(136, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Matemáticas";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(37, 62);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Nombre del alumno";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(37, 88);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 3;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(27, 125);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(147, 23);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Calificaciones de asignaturas";
+			// 
+			// Ecosistemas
+			// 
+			this.Ecosistemas.Location = new System.Drawing.Point(49, 148);
+			this.Ecosistemas.Name = "Ecosistemas";
+			this.Ecosistemas.Size = new System.Drawing.Size(67, 23);
+			this.Ecosistemas.TabIndex = 5;
+			this.Ecosistemas.Text = "Ecosistemas";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(49, 188);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 23);
+			this.label5.TabIndex = 6;
+			this.label5.Text = "Humanidades";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(49, 229);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(100, 23);
+			this.label6.TabIndex = 7;
+			this.label6.Text = "Ingles";
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(49, 268);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(100, 23);
+			this.label7.TabIndex = 8;
+			this.label7.Text = "Fisica";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(37, 165);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 9;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(37, 206);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 10;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(37, 245);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 11;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(37, 286);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 12;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(228, 148);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 13;
+			this.label4.Text = "Promedio";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(228, 165);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 20);
+			this.textBox6.TabIndex = 14;
+			// 
+			// label8
+			// 
+			this.label8.Location = new System.Drawing.Point(228, 188);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(115, 23);
+			this.label8.TabIndex = 15;
+			this.label8.Text = "Aprobado/Repobado";
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(228, 206);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(100, 20);
+			this.textBox7.TabIndex = 16;
+			// 
+			// label9
+			// 
+			this.label9.Location = new System.Drawing.Point(228, 228);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(100, 23);
+			this.label9.TabIndex = 17;
+			this.label9.Text = "No. Mayor";
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(228, 244);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(100, 20);
+			this.textBox8.TabIndex = 18;
+			// 
+			// label10
+			// 
+			this.label10.Location = new System.Drawing.Point(228, 268);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(100, 23);
+			this.label10.TabIndex = 19;
+			this.label10.Text = "No. Menor";
+			// 
+			// textBox9
+			// 
+			this.textBox9.Location = new System.Drawing.Point(228, 285);
+			this.textBox9.Name = "textBox9";
+			this.textBox9.Size = new System.Drawing.Size(100, 20);
+			this.textBox9.TabIndex = 20;
+			// 
+			// label11
+			// 
+			this.label11.Location = new System.Drawing.Point(373, 148);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(100, 23);
+			this.label11.TabIndex = 21;
+			this.label11.Text = "Media";
+			// 
+			// textBox10
+			// 
+			this.textBox10.Location = new System.Drawing.Point(373, 165);
+			this.textBox10.Name = "textBox10";
+			this.textBox10.Size = new System.Drawing.Size(100, 20);
+			this.textBox10.TabIndex = 22;
+			// 
+			// label12
+			// 
+			this.label12.Location = new System.Drawing.Point(373, 188);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(136, 23);
+			this.label12.TabIndex = 23;
+			this.label12.Text = "Cantidad de reprobados";
+			// 
+			// textBox11
+			// 
+			this.textBox11.Location = new System.Drawing.Point(373, 206);
+			this.textBox11.Name = "textBox11";
+			this.textBox11.Size = new System.Drawing.Size(121, 20);
+			this.textBox11.TabIndex = 24;
+			// 
+			// label13
+			// 
+			this.label13.Location = new System.Drawing.Point(373, 229);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(121, 23);
+			this.label13.TabIndex = 25;
+			this.label13.Text = "Cantidad de aprobados";
+			// 
+			// textBox12
+			// 
+			this.textBox12.Location = new System.Drawing.Point(373, 245);
+			this.textBox12.Name = "textBox12";
+			this.textBox12.Size = new System.Drawing.Size(121, 20);
+			this.textBox12.TabIndex = 26;
 			// 
 			// MATEMATICAS
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "MATEMATICAS";
+			this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+			this.ClientSize = new System.Drawing.Size(888, 336);
+			this.Controls.Add(this.textBox12);
+			this.Controls.Add(this.label13);
+			this.Controls.Add(this.textBox11);
+			this.Controls.Add(this.label12);
+			this.Controls.Add(this.textBox10);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.textBox9);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.textBox8);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.Ecosistemas);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
 			this.Name = "MATEMATICAS";
+			this.Text = "MATEMATICAS";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+		private System.Windows.Forms.TextBox textBox12;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.TextBox textBox11;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.TextBox textBox10;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label Ecosistemas;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox1;
 	}
 }
--- GERMANSITO/GERMANSITO/MATEMATICAS.cs
+++ GERMANSITO/GERMANSITO/MATEMATICAS.cs
@@ -27,6 +27,95 @@ namespace GERMANSITO
 			//
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
+			
+			
+			 Console.WriteLine("Nombre del alumno");
+    string na; //nombre del alumno.
+    na=Console.ReadLine();
+
+    Console.WriteLine("Calificacion de Ecologia");
+    int cal1=Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Calificacion de Fisica");
+    int cal2;
+    cal2=Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Calificacion de Humanidades");
+    int cal3;
+    cal3=Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Calificacion de Ingles");
+    int cal4;
+    cal4=Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Calificacion de Matematicas");
+    int cal5;
+    cal5=Convert.ToInt32(Console.ReadLine());
+
+    int promedio=(cal1 + cal2 + cal3 + cal4+ cal5)/5;
+    Console.WriteLine("Promedio="+promedio);
+
+    if(promedio<6){
+        Console.WriteLine("Reprobado");
+    }
+    else{
+        if(promedio>=6){
+            Console.WriteLine("Aprobado");
+        }
+    }
+
+            //MAYOR Y MENOR.
+
+    if(cal1>cal2 && cal1>cal3 && cal1>cal4 && cal1>cal5){
+        Console.WriteLine("El numero mayor es"+cal1);
+    }
+    else if(cal2>cal1 && cal2>cal3 && cal2>cal4 && cal2>cal5){
+        Console.WriteLine("El numero mayor es"+cal2);
+    }
+    else if(cal3>cal1 && cal3>cal2 && cal3>cal4 && cal2>cal5){
+        Console.WriteLine("El numero mayor es"+cal2);
+    }
+    else if(cal4>cal1 && cal4>cal2 && cal4>cal3 && cal4>cal5){
+        Console.WriteLine("El numero mayor es"+cal2);
+    }
+    else if(cal5>cal1 && cal5>cal2 && cal5>cal3 && cal2>cal4){
+        Console.WriteLine("El numero mayor es"+cal2);
+    }
+
+
+
+    if(cal1<cal2 && cal1<cal3 && cal1<cal4 && cal1<cal5){
+        Console.WriteLine("El numero menor es"+cal1);
+    }
+    else if(cal2<cal1 && cal2<cal3 && cal2<cal4 && cal2<cal5){
+        Console.WriteLine("El numero menor es"+cal2);
+    }
+    else if(cal3<cal1 && cal3<cal2 && cal3<cal4 && cal2<cal5){
+        Console.WriteLine("El numero menor es"+cal2);
+    }
+    else if(cal4<cal1 && cal4<cal2 && cal4<cal3 && cal4<cal5){
+        Console.WriteLine("El numero menor es"+cal2);
+    }
+    else if(cal5<cal1 && cal5<cal2 && cal5<cal3 && cal2<cal4){
+        Console.WriteLine("El numero menor es"+cal2);
+    }
+
+            //MEDIA.
+    double media=(cal1+cal2+cal3+cal4+cal5)/5.0;
+    Console.WriteLine("La media es"+media);
+
+            //CANTIDAD DE REPROBADOS Y APROBADOS.
+    int CantRepro = 0, CantApro = 0;
+            Console.WriteLine("Nota obtenida:");
+            double nota = Convert.ToDouble(Console.ReadLine());
+            if (nota < 6) {
+                CantRepro++;
+            } else {
+                CantApro++;
+            }
+
+            Console.WriteLine("Cantidad de aprobados es: " + CantApro);
+            Console.WriteLine("Cantidad de reprobados es: " + CantRepro);
 		}
 	}
 }
