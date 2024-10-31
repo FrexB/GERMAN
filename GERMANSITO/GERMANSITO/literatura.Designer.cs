131f07ef819257e035f3dc0c7c30ff32e5cd5e4d
Ramsesballesteros <badr080626hmnlzma7@cbtis217.edu.mx>
2024-10-31 08:21:10 -0600

    Prueba subiendo humanidades sin carpetas debug

--- GERMANSITO/GERMANSITO/literatura.Designer.cs
+++ GERMANSITO/GERMANSITO/literatura.Designer.cs
@@ -1,14 +1,14 @@
 ﻿/*
  * Created by SharpDevelop.
- * User: CC2_PC26
- * Date: 29/10/2024
- * Time: 08:07 a. m.
+ * User: CC2_PC28
+ * Date: 31/10/2024
+ * Time: 07:59 a. m.
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 namespace GERMANSITO
 {
-	partial class HUMANIDADES
+	partial class literatura
 	{
 		/// <summary>
 		/// Designer variable used to keep track of non-visual components.
@@ -36,79 +36,67 @@ namespace GERMANSITO
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HUMANIDADES));
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(literatura));
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// pictureBox1
 			// 
 			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.Location = new System.Drawing.Point(4, -7);
+			this.pictureBox1.Location = new System.Drawing.Point(-2, -3);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(762, 339);
+			this.pictureBox1.Size = new System.Drawing.Size(553, 265);
 			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
 			// 
 			// label1
 			// 
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(330, 9);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(164, 31);
+			this.label1.Size = new System.Drawing.Size(156, 64);
 			this.label1.TabIndex = 1;
-			this.label1.Text = "HUMANIDADES";
+			this.label1.Text = "literatura";
+			this.label1.Click += new System.EventHandler(this.Label1Click);
 			// 
 			// label2
 			// 
-			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(12, 109);
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(302, 61);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(79, 22);
+			this.label2.Size = new System.Drawing.Size(240, 91);
 			this.label2.TabIndex = 2;
-			this.label2.Text = "Filosofia";
+			this.label2.Text = "La filosofía y las humanidades están estrechamente relacionadas, ya que ambas dis" +
+			"ciplinas exploran aspectos fundamentales de la condición humana.";
 			this.label2.Click += new System.EventHandler(this.Label2Click);
 			// 
 			// label3
 			// 
-			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(12, 153);
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(57, 166);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(79, 28);
+			this.label3.Size = new System.Drawing.Size(485, 71);
 			this.label3.TabIndex = 3;
-			this.label3.Text = "Literatura";
-			this.label3.Click += new System.EventHandler(this.Label3Click);
+			this.label3.Text = resources.GetString("label3.Text");
 			// 
-			// label4
-			// 
-			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label4.Location = new System.Drawing.Point(12, 198);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(79, 23);
-			this.label4.TabIndex = 4;
-			this.label4.Text = "Historia";
-			this.label4.Click += new System.EventHandler(this.Label4Click);
-			// 
-			// HUMANIDADES
+			// literatura
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(763, 334);
-			this.Controls.Add(this.label4);
+			this.ClientSize = new System.Drawing.Size(554, 261);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.pictureBox1);
-			this.Name = "HUMANIDADES";
-			this.Text = "HUMANIDADES";
+			this.Name = "literatura";
+			this.Text = "literatura";
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 		}
-		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label1;
